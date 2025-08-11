ecb20feb3f2919df72145b17099c715438c91dd3
root <lapique.philippe@gmail.com>
2025-08-11 18:52:19 +0000

    Add file read_line.c : progs for prints it on the next line

--- Exercice/read_line.c
+++ Exercice/read_line.c
@@ -1,14 +1,32 @@
-#include <stdio.h>
-#include <unisd.h>
+#include "main.h"
 
- /**
+/**
+ * main - Entry point for the simple getline example
  *
+ * This program displays a prompt "$ ", waits for user input,
+ * reads a full line from stdin using getline(), and then
+ * prints back the line entered by the user.
  *
- *
- *
+ * Return: Always 0 on success, or -1 on failure.
  */
 
-int main(int argc, char**argv)
+int main(void)
 {
-getline(
-p
+size_t buffer = 0;
+char *string = NULL;
+ssize_t temp;
+
+printf("$ ");
+temp = getline(&string, &buffer, stdin);
+
+if (temp == -1)
+	{
+	printf("\n");
+	}
+else
+	{
+	printf("%s", string);
+	}
+free(string);
+return (0);
+}
