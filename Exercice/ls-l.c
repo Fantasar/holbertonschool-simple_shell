59c9a80f51ab7a41c8d549d29455ec142519d23a
root <lapique.philippe@gmail.com>
2025-08-12 12:03:15 +0000

    Add file : ls-l.c for prints five ls -l in /tmp

---
+++ Exercice/ls-l.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+extern char **environ;
+
+int main(void) {
+    int count = 0;
+    pid_t enfant;
+
+    while (count <= 5) {
+        enfant = fork();
+
+        if (enfant == -1) {
+            perror("fork");
+            return 1;
+        }
+
+        if (enfant == 0) {
+            char *args[] = { "ls", "-l", "/tmp", NULL };
+            execve("/bin/ls", args, environ);
+            perror("execve");
+            return 1;
+        } else {
+            wait(NULL);
+            count++;
+        }
+    }
+
+    return 0;
+}
