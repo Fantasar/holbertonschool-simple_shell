df3c77f540d75b30cb96f9d5f64f8c2c25874c1a
root <lapique.philippe@gmail.com>
2025-08-12 12:07:07 +0000

    Modif main.h : Add prototype for function Wait | fork | Execve

--- Exercice/main.h
+++ Exercice/main.h
@@ -6,9 +6,14 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /*===== Prototypes =====*/
 
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 char *strtok(char *str, const char *delim);
+pid_t fork(void);
+int execve(const char *pathname, char *const argv[], char *const envp[]);
+pid_t wait(int *wstatus);
 #endif
