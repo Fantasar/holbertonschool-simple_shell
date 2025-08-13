01c33027ca5c405d7fe387a08e69d97fc66ced4f
root <lapique.philippe@gmail.com>
2025-08-13 13:12:21 +0000

    Add file _getenv.c : progs for gets an environment variable

---
+++ Exercice/_getenv.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _getenv - retrieves the value of an environment variable
+ * @name: the name of the environment variable to search for
+ *
+ * This function loops through the environment variables stored in the
+ * global variable `environ`. For each entry, it compares the beginning
+ * of the string with the given `name`. If the name matches and the next
+ * character is '=', the function returns a pointer to the value part of
+ * the environment variable (the string after the '=' sign).
+ *
+ * Return: pointer to the value string if found, NULL otherwise.
+ */
+
+extern char **environ;
+
+char *_getenv(const char *name)
+{
+char *variable;
+int taille;
+char next_char;
+size_t len;
+int index = 0;
+
+if (name == NULL)
+	{
+	return (NULL);
+	}
+len = strlen(name);
+while (environ[index] != NULL)
+	{
+	variable = environ[index];
+	taille = strcmp(variable, name, len);
+	next_char = variable[len];
+
+	if (taille == 0 && nex_char == '=')
+		{
+		return (var + len + 1);
+		}
+
+	index++;
+	}
+retunr (NULL);
+}
