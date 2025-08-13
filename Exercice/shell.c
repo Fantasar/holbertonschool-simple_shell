7b335f42de2be6eb08c3787cf4602cd01947e1aa
root <lapique.philippe@gmail.com>
2025-08-13 07:12:31 +0000

    Modif shell.c : function argv[64] for execute other arguments users

--- Exercice/shell.c
+++ Exercice/shell.c
@@ -1,58 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include "main.h"
+
+/**
+ * main - Simple shell that executes commands given with their full path
+ * @void: No arguments are passed to main
+ *
+ * This program implements a minimal interactive shell.
+ * It continuously:
+ *   - Displays a prompt ("$ ")
+ *   - Reads a line of input from the user
+ *   - Parses the command into program name and arguments
+ *   - Creates a child process using fork()
+ *   - Executes the given program in the child process using execve()
+ *   - Waits for the child process to finish before prompting again
+ *
+ * The shell only accepts commands given with their full path
+ * (e.g., "/bin/ls -l"), and uses the current environment variables.
+ *
+ * Return: Always 0 on success, or 1 on failure.
+ */
 
 extern char **environ;
 
 int main(void)
 {
-    char *temp = NULL;
-    size_t num = 0;
-    ssize_t len;
-    pid_t user;
+char *temp = NULL;
+size_t num = 0;
+ssize_t len;
+pid_t user;
+int index = 0;
+char *argv[64];
 
-    while (1)
-    {
-        printf("$ ");
-        len = getline(&temp, &num, stdin);
+while (1)
+	{
+	printf("$ ");
+	len = getline(&temp, &num, stdin);
 
-        if (len == -1)
-        {
-            printf("\n");
-            break;
-        }
+	if (len == -1)
+		{
+		printf("\n");
+		break;
+		}
 
-        temp[strcspn(temp, "\n")] = '\0';
+	temp[strcspn(temp, "\n")] = '\0';
 
-        if (temp[0] == '\0')
-            continue;
+	if (temp[0] == '\0')
+		continue;
 
-        user = fork();
+	user = fork();
 
-        if (user == -1)
-        {
-            perror("fork");
-            free(temp);
-            return 1;
-        }
+	if (user == -1)
+		{
+		perror("fork");
+		free(temp);
+		return (1);
+		}
 
-        if (user == 0)
-        {
-            char *argv[] = { temp, NULL };
-            if (execve(argv[0], argv, environ) == -1)
-            {
-                perror("execve");
-                exit(1);
-            }
-        }
-        else
-        {
-            wait(NULL);
-        }
-    }
+	if (user == 0)
+		{
+		char *token = strtok(temp, " ");
+		while (token != NULL && index < 63)
+			{
+			argv[index++] = token;
+			token = strtok(NULL, " ");
+			}
+		argv[index] = NULL;
+		execve(argv[0], argv, environ);
+		perror("execve");
+		exit(1);
+		}
+	else
+		{
+		wait(NULL);
+		}
+	}
 
-    free(temp);
-    return 0;
+free(temp);
+return (0);
 }
