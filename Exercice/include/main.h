4fda3a18816328ff8f947fc85b6c60a34a474b66
root <lapique.philippe@gmail.com>
2025-08-13 16:03:41 +0000

    Add Prototype new_PATH_list && structure path_node

--- Exercice/include/main.h
+++ Exercice/include/main.h
@@ -10,6 +10,20 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+/**
+ * struct path_node - singly linked list for PATH directories
+ * @dir: directory path
+ * @next: pointer to next node
+ */
+
+/*==== Type Definitions ====*/
+
+typedef struct path_node
+{
+    char *dir;
+    struct path_node *next;
+} path_node_t;
+
 /*===== Prototypes =====*/
 
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
@@ -21,4 +35,5 @@ int fprintf(FILE *stream, const char *format, ... );
 char *_getenv(const char *name);
 int strncmp(const char *s1, const char *s2, size_t n);
 void directory_PATH(char *env);
+path_node_t *new_PATH_list(void);
 #endif
