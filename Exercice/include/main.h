101e2292cfdf428f7fb6f7ace71b882f6e05c5e4
root <lapique.philippe@gmail.com>
2025-08-14 18:59:39 +0000

    Modif main.h : Prototype add variable in env

--- Exercice/include/main.h
+++ Exercice/include/main.h
@@ -36,5 +36,7 @@ char *_getenv(const char *name);
 int strncmp(const char *s1, const char *s2, size_t n);
 void directory_PATH(char *env);
 path_node_t *new_PATH_list(void);
+int _setenv(const char *name, const char *value, int overwrite);
+
 
 #endif
