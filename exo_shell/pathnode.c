1bd012d2c466d574a35c60005d21438a643fecb6
Ilnnn <camoreze@gmail.com>
2025-08-15 10:00:45 +0000

    betty correction and 40 lines clear

--- exo_shell/pathnode.c
+++ exo_shell/pathnode.c
@@ -1,66 +1,41 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
-
 /**
- * new_PATH_list - Builds a linked list of the directories in the PATH variable
+ * new_PATH_list - Builds a linked list of directories from the PATH variable
  *
- * Return: A pointer to the head of the linked list, or NULL on failure.
+ * Return: Pointer to the head of the linked list, or NULL if it fails
  */
-extern char **environ;
 
 path_node_t *new_PATH_list(void)
 {
-	char *path_value;
-	char *path_copy;
-	char *cut;
-	path_node_t *temp;
-	path_node_t *head = NULL;
-	path_node_t *tail = NULL;
+	char *path_copy, *cut;
+	path_node_t *head = NULL, *tail = NULL, *temp;
 
-	path_value = _getenv("PATH");
+	path_copy = strdup(_getenv("PATH"));
+	if (!path_copy)
+	return (NULL);
 
-	if (path_value == NULL)
-		return (NULL);
-
-		path_copy = strdup(path_value);
-	if (path_copy == NULL)
-		return (NULL);
-
-		cut = strtok(path_copy, ":");
-	while (cut != NULL)
+	cut = strtok(path_copy, ":");
+	while (cut)
 	{
-		temp = malloc(sizeof(path_node_t));
-		if (temp == NULL)
-		
-		{
-			free(path_copy);
-			return (NULL);
-		}
-		temp->dir = strdup(cut);
-		if (temp->dir == NULL)
-		{
-			free(temp);
-			free(path_copy);
-			return (NULL);
-		}
-
-		temp->next = NULL;
-
-		if (head == NULL)
-		{
-			head = temp;
-			tail = temp;
-		}
-		else
-		{
-		tail->next = temp;
-		tail = temp;
-		}
-
-		cut = strtok(NULL, ":");
+	temp = malloc(sizeof(*temp));
+		if (!temp)
+		break;
+	temp->dir = strdup(cut);
+	if (!temp->dir)
+	{
+		free(temp);
+		break;
 	}
-
+	temp->next = NULL;
+	if (!head)
+	head = tail = temp;
+	else
+	tail = tail->next = temp;
+	cut = strtok(NULL, ":");
+	}
 	free(path_copy);
 	return (head);
 }
+
