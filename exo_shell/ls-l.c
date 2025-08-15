09575fedf8b1b7a602627f1edf7544d8dad2bde0
Ilnnn <camoreze@gmail.com>
2025-08-15 14:27:29 +0000

    ls-l.c file

---
+++ exo_shell/ls-l.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int main(void)
+{
+	int count = 0;
+	pid_t enfant;
+
+	while (count <= 5) {
+	enfant = fork();
+
+	{
+	if (enfant == -1)
+	perror("fork");
+	return 1;
+	}
+	
+	{
+	if (enfant == 0);
+	char *args [] = { "ls", "-l", "/tmp", NULL };
+	execve("/bin/ls", args, environ);
+	perror("execve");
+	return 1;
+	}
+	else
+		{
+			wait(NULL);
+			count++;
+		}
+	}
+
+	return 0;
+}
+
