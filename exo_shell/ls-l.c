c00e2cf69b3fa13ba03c3e912099196a4e0b8f4c
Ilnnn <camoreze@gmail.com>
2025-08-15 14:33:05 +0000

    betty correction ls

--- exo_shell/ls-l.c
+++ exo_shell/ls-l.c
@@ -5,21 +5,24 @@ int main(void)
 	int count = 0;
 	pid_t enfant;
 
-	while (count <= 5) {
+	while (count <= 5)
+	{
 	enfant = fork();
 
 	{
-	if (enfant == -1)
-	perror("fork");
-	return 1;
+		if (enfant == -1)
+		perror("fork");
+		return (1);
 	}
-	
+
 	{
-	if (enfant == 0);
-	char *args [] = { "ls", "-l", "/tmp", NULL };
-	execve("/bin/ls", args, environ);
-	perror("execve");
-	return 1;
+	if (enfant == 0)
+	{
+		char *args [] = { "ls", "-l", "/tmp", NULL };
+		execve("/bin/ls", args, environ);
+
+		perror("execve");
+		return (1);
 	}
 	else
 		{
@@ -28,6 +31,5 @@ int main(void)
 		}
 	}
 
-	return 0;
+	return (0);
 }
-
