beb8c701af69e3d3fb014dc4b80d72bfac7ddd3a
Ilnnn <camoreze@gmail.com>
2025-08-15 09:40:10 +0000

    betty spaces corrections pathnode

--- exo_shell/pathnode.c
+++ exo_shell/pathnode.c
@@ -11,54 +11,56 @@ extern char **environ;
 
 path_node_t *new_PATH_list(void)
 {
-    char *path_value;
-    char *path_copy;
-    char *cut;
-    path_node_t *temp;
-    path_node_t *head = NULL;
-    path_node_t *tail = NULL;
+	char *path_value;
+	char *path_copy;
+	char *cut;
+	path_node_t *temp;
+	path_node_t *head = NULL;
+	path_node_t *tail = NULL;
 
-    path_value = _getenv("PATH");
-    if (path_value == NULL)
-        return (NULL);
+	path_value = _getenv("PATH");
 
-    path_copy = strdup(path_value);
-    if (path_copy == NULL)
-        return (NULL);
+	if (path_value == NULL)
+		return (NULL);
 
-    cut = strtok(path_copy, ":");
-    while (cut != NULL)
-    {
-        temp = malloc(sizeof(path_node_t));
-        if (temp == NULL)
-        {
-            free(path_copy);
-            return (NULL);
-        }
-        temp->dir = strdup(cut);
-        if (temp->dir == NULL)
-        {
-            free(temp);
-            free(path_copy);
-            return (NULL);
-        }
-        temp->next = NULL;
+		path_copy = strdup(path_value);
+	if (path_copy == NULL)
+		return (NULL);
 
-        if (head == NULL)
-        {
-            head = temp;
-            tail = temp;
-        }
-        else
-        {
-            tail->next = temp;
-            tail = temp;
-        }
+		cut = strtok(path_copy, ":");
+	while (cut != NULL)
+	{
+		temp = malloc(sizeof(path_node_t));
+		if (temp == NULL)
+		
+		{
+			free(path_copy);
+			return (NULL);
+		}
+		temp->dir = strdup(cut);
+		if (temp->dir == NULL)
+		{
+			free(temp);
+			free(path_copy);
+			return (NULL);
+		}
 
-        cut = strtok(NULL, ":");
-    }
+		temp->next = NULL;
 
-    free(path_copy);
-    return (head);
+		if (head == NULL)
+		{
+			head = temp;
+			tail = temp;
+		}
+		else
+		{
+		tail->next = temp;
+		tail = temp;
+		}
+
+		cut = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (head);
 }
-
