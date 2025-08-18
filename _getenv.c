3c12adbccbdfdbbd83a89d621a598eb6beecfa49
root <lapique.philippe@gmail.com>
2025-08-18 22:42:06 +0000

    Add file _getenv : function searche the environment list to find the environment variable name, and returns a pointer to the corresponding value string. For the Progs Shell Terminal

---
+++ _getenv.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _getenv - retrieves the value of an environment variable
+ * @name: the name of the environment variable to search for
+ *
+ * Return: pointer to the value string if found, NULL otherwise.
+ */
+
+char *_getenv(const char *name)
+{
+	size_t len;
+	int index;
+
+	if (name == NULL)
+		{
+		return (NULL);
+		}
+
+	len = strlen(name);
+
+	for (index = 0; environ[index] != NULL; index++)
+	{
+		if (strncmp(environ[index], name, len) == 0
+			&& environ[index][len] == '=')
+			{
+			return (environ[index] + len + 1);
+			}
+	}
+
+	return (NULL);
+]
