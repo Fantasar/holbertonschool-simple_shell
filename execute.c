4b3d4557585e0a3a6fe28fa6e4c0ba72b4394fd5
root <lapique.philippe@gmail.com>
2025-08-18 21:04:59 +0000

    Add file execute.c : Function for exe command user '/' or find in PATH. It's for reals Shell Terminal

--- execute.c
+++ execute.c
@@ -1,1 +1,30 @@
-#include
+#include "main.h"
+
+/**
+ * execute_command - Run a command given by the user
+ * @argv: array of arguments (command + options)
+ * @environ: environment variables
+ *
+ * This function checks the user command. If the command
+ * has a '/' character, it is executed directly with execve.
+ * Otherwise, it calls directory_PATH to search for the
+ * command inside the PATH directories.
+ */
+
+void execute_command(char *argv[], char **environ)
+{
+	if (argv[0] == NULL)
+	{
+		return;
+	}
+
+	if (strchr(argv[0], '/') != NULL)
+	{
+		execve(argv[0], argv, environ);
+		perror("execve");
+	}
+	else
+	{
+		find_PATH(argv, environ);
+	}
+}
