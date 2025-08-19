6295442f439b02e08f34f440dc062f81acc9ceaf
root <lapique.philippe@gmail.com>
2025-08-19 00:07:40 +0000

    Modif file main.c : Modif main.c for check V.1 Shell

--- main.c
+++ main.c
@@ -1,56 +1,54 @@
 #include "main.h"
 
 /**
- * main - Simple shell program
+ * main - Simple shell 0.1
  * Return: 0 on success
  *
- * This program shows a prompt, waits for a user command,
- * splits it into arguments, and runs it.
- * It uses fork to create a child process,
- * execve to run the command, and wait to pause
- * until the child finishes.
+ * This shell:
+ * - displays a prompt
+ * - reads a single command line (no arguments)
+ * - executes it only if the full path is provided
+ * - handles EOF (Ctrl+D)
  */
-
 int main(void)
 {
-	char *temp = NULL;
-	size_t num = 0;
-	ssize_t len;
-	pid_t user;
-	char *argv[64];
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read_len;
+	char *argv[2];
 
 	while (1)
 	{
-		printf("$ ");
-		len = getline(&temp, &num, stdin);
-		if (len == -1)
+		printf("#cisfun$ ");
+		fflush(stdout);
+
+		read_len = getline(&line, &len, stdin);
+		if (read_len == -1)
 		{
 			printf("\n");
 			break;
 		}
-		temp[strcspn(temp, "\n")] = '\0';
-		if (temp[0] == '\0')
-		continue;
-		cut_command(temp, argv);
 
-		if (handle_builtin(argv))
+		line[read_len - 1] = '\0';
+
+		if (line[0] == '\0')
 			continue;
-		user = fork();
-		if (user == -1)
+
+		argv[0] = line;
+		argv[1] = NULL;
+
+		if (fork() == 0)
 		{
-			perror("fork");
-			continue;
-		}
-		if (user == 0)
-		{
-			execute_command(argv, environ);
-			exit(1);
+			execve(argv[0], argv, environ);
+			perror("./shell");
+			_exit(1);
 		}
 		else
 		{
 			wait(NULL);
 		}
 	}
-	free(temp);
+
+	free(line);
 	return (0);
 }
