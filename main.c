ac515a0828d34c8a684afe13e0b4393bfedf8639
root <lapique.philippe@gmail.com>
2025-08-19 14:21:03 +0000

    Modif main.c : add function check_user for values of return user

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include "main.h"
 
 /**
+ * check_user - Handle the result of fork and execute the command
+ * @user: PID returned by fork()
+ * @argv: array of arguments (command + options)
+ * @environ: environment variables
+ *
+ * Description: This function checks the result of fork().
+ * - If fork() fails, it prints an error message and returns.
+ * - If in the child process, it calls execute_command() with argv
+ *   and environ, and exits with status 1 if execution fails.
+ * The parent process is not handled here, it should be managed
+ * in the caller (main).
+ *
+ * Return: Nothing (void).
+ */
+
+void check_user(int user, char *argv[], char **environ)
+{
+	if (user == -1)
+	{
+		perror("fork");
+		return;
+	}
+
+	if (user == 0)
+	{
+		execute_command(argv, environ);
+		exit(1);
+	}
+}
+
+/**
  * main - Simple shell 0.1
  * Return: 0 on success
  *
@@ -10,6 +41,7 @@
  * - executes it only if the full path is provided
  * - handles EOF (Ctrl+D)
  */
+
 int main(void)
 {
 	char *temp = NULL;
@@ -26,34 +58,23 @@ int main(void)
 			fflush(stdout);
 		}
 		len = getline(&temp, &num, stdin);
-
 		if (len == -1)
-		{
 			break;
-		}
-
 		temp[strcspn(temp, "\n")] = '\0';
 
 		if (temp[0] == '\0')
 			continue;
 
-		user = fork();
-		if (user == -1)
-		{
-			perror("fork");
+		cut_command(temp, argv);
+		if (handle_builtin(argv))
 			continue;
-		}
-		if (user == 0)
-		{
-			cut_command(temp, argv);
-			execute_command(argv, environ);
-			exit(1);
- 		}
-		else
-		{
+
+		user = fork();
+		check_user(user, argv, environ);
+
+		if (user > 0)
 			wait(NULL);
-		}
 	}
 	free(temp);
-	return 0;
+	return (0);
 }
